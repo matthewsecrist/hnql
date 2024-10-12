17d78e19a158fc17982425338fb097858433bb1d
Matthew Secrist <matthew@matthewsecrist.net>
2024-10-11 21:54:56 -0500

    Apollo Explorer in Prod

--- src/routes/graph.ts
+++ src/routes/graph.ts
@@ -1,17 +1,28 @@
 import type { BaseContext } from '@apollo/server'
 import { ApolloServer } from '@apollo/server'
 import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default'
+import {
   fastifyApolloDrainPlugin,
   fastifyApolloHandler,
 } from '@as-integrations/fastify'
 import fp from 'fastify-plugin'
 import { context, resolvers, typeDefs } from '../graph'
+import { type ServiceEnv, env } from '../utils/env'
 
 export default fp(async (fastify, opts) => {
+  const landingPage =
+    env<ServiceEnv>('SERVICE_ENV', 'dev') === 'production'
+      ? // @ts-expect-error it doesn't like missing graphRef
+        ApolloServerPluginLandingPageProductionDefault({ embed: true })
+      : ApolloServerPluginLandingPageLocalDefault({})
+
   const apollo = new ApolloServer<BaseContext>({
     typeDefs: typeDefs,
     resolvers: resolvers,
-    plugins: [fastifyApolloDrainPlugin(fastify)],
+    plugins: [fastifyApolloDrainPlugin(fastify), landingPage],
   })
 
   await apollo.start()
