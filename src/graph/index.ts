dd101a98ba4773c95ef941700dc4a160873fb044
Matthew Secrist <matthew@matthewsecrist.net>
2024-10-11 21:19:08 -0500

    Deployment to Render (#3)
    * Deployment to Render
    
    * oops

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -1,9 +1,12 @@
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
-export const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), {
-  encoding: 'utf-8',
-})
+export const typeDefs = readFileSync(
+  path.join(__dirname, '../../schema.graphql'),
+  {
+    encoding: 'utf-8',
+  },
+)
 
 export { type Context, context } from './context'
 export { resolvers } from './resolvers'
